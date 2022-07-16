1e614086bff3bc4320eb142b7b6ab408cfba6883
manikandan190 <ramakrishnanmanikandan2000@gmail.com>
2022-07-17 02:10:48 +0530

    first commit

--- src/reducers/auth.reducer.js
+++ src/reducers/auth.reducer.js
@@ -26,7 +26,7 @@ switch(action.type){
     case authConstants.LOGIN_SUCCESS:
         state={
             ...state,
-            user:action.payload.user,
+           
             token:action.payload.token,
             authenticate:true,
             authenticating:false,
